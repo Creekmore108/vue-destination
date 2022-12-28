bee9fef4284a3a8f30a3ad689da65204b4d08b91
Steve Creekmore <Creekmore108@gmail.com>
2022-12-28 16:20:52 -0700

    added images

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,46 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "details/:id",
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: () =>
+      import(/* webpackChunkName: "brazil" */ "../views/BrazilView.vue"),
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: () =>
+      import(/* webpackChunkName: "jamaica" */ "../views/JamaicaView.vue"),
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: () =>
+      import(/* webpackChunkName: "panama" */ "../views/PanamaView.vue"),
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: () =>
+      import(/* webpackChunkName: "hawaii" */ "../views/HawaiiView.vue"),
+  },
+  {
+    path: "/details/:id",
     name: "DestinationDetails",
     component: DestinationDetails,
     // component: () =>
-    //   import(/* webpackChunkName: "DestinationDetails"*/ "../views/DestinationDetails.vue")
+    //   import(
+    //     /* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails.vue"
+    //   ),
   },
 ];
 
